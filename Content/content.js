2d9145824d374d15e77247dc4347a667138f86df
RatPhat <stenkaal@hotmail.com>
2023-04-17 21:22:47 +0200

    Added new features  and fixed somee css issues
    Added darkMode and made sure content was in the right place.

---
+++ Content/content.js
@@ -0,0 +1,25 @@
+function darkMode() {
+    var elem = document.getElementById("body");
+    var button = document.getElementById("thisButton1");
+    var auth = document.getElementById("auth");
+
+    console.log(button.value)
+    if (button.value == "Dark Mode") 
+    {
+        button.value = "Light Mode";
+        elem.style.backgroundColor = "#212121";
+        button.style.color = "#ffffff";
+        button.style.backgroundColor = "#000000";
+        auth.style.color = "#ffffff";
+        auth.style.backgroundColor = "#000000";
+    }
+    else 
+    {
+        button.value = "Dark Mode";
+        elem.style.backgroundColor = "#ffffff";
+        button.style.color = "#000000";
+        button.style.backgroundColor = "#a0a0a0";
+        auth.style.color = "#000000";
+        auth.style.backgroundColor = "#a0a0a0";
+    }
+}
